28527fc86af9a9551ce1a01cdfc52369f5331531
Matthias Clausen <matthiasclausen@posteo.de>
2024-03-14 10:45:03 +0100

    qr scan fails better

--- src/hotpants/ScanForm.java
+++ src/hotpants/ScanForm.java
@@ -1,19 +1,26 @@
 package hotpants;
 
 import hotpants.qr.DecodeCanvas;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Form;
 import javax.microedition.lcdui.Gauge;
 import javax.microedition.lcdui.Image;
 import javax.microedition.media.*;
 import javax.microedition.media.control.VideoControl;
 
-public class ScanForm extends Form {
+public class ScanForm extends Form implements CommandListener {
     private final Midlet midlet;
     private Gauge progress;
+    private final Command backCmd;
     
     public ScanForm(Midlet m) {
         super("Scan QR Code");
+        backCmd = new Command("Back", Command.BACK, 1);
+        this.addCommand(backCmd);
+        this.setCommandListener(this);
         midlet = m;
     }
     
@@ -23,15 +30,16 @@ public class ScanForm extends Form {
         final ScanForm scanForm = this;
         new Thread(new Runnable() {       
             public void run() { 
-                dk.onlinecity.qrr.client.CameraControl cameraControl = dk.onlinecity.qrr.client.CameraControl.getInstance();
-                byte[] snapShot = cameraControl.getSnapshot();
-                Player player = cameraControl.getPlayer();
                 try {
-                        player.stop();
-                } catch (MediaException e) {
+                    dk.onlinecity.qrr.client.CameraControl cameraControl = dk.onlinecity.qrr.client.CameraControl.getInstance();
+                    byte[] snapShot = cameraControl.getSnapshot();
+                    Player player = cameraControl.getPlayer();
+                    player.stop();
+                    DecodeCanvas decodeCanvas = new DecodeCanvas(midlet, scanForm, snapShot);
+                    Display.getDisplay(midlet).setCurrent(decodeCanvas);
+                } catch (Exception e) {
+                    showCamNotPossible(e.getMessage());
                 }
-                DecodeCanvas decodeCanvas = new DecodeCanvas(midlet, scanForm, snapShot);
-                Display.getDisplay(midlet).setCurrent(decodeCanvas);
             } 
         }).run();
     }
@@ -51,7 +59,13 @@ public class ScanForm extends Form {
             Display.getDisplay(midlet).setCurrent(cameraCanvas);
         } catch (MediaException e) {
             e.printStackTrace();
+            showCamNotPossible(e.getMessage());
         }
+    }
+    
+    private void showCamNotPossible(String msg) {
+        deleteAll();
+        append("Scan not possible. " + msg);
     }
     
     public void setThumbnail(Image thumbnail) {
@@ -63,5 +77,11 @@ public class ScanForm extends Form {
     
     public void setProgress(int percentage) {
         progress.setValue(percentage);
+    }
+
+    public void commandAction(Command c, Displayable d) {
+        if (c == backCmd) {
+            midlet.showMainForm();
+        }
     }
 }
